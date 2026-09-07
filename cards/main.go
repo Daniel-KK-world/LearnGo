ccde5e64e1d3b5ec38eb7327f3eb143177005a9b
Daniel P.K <danilofletcher978@gmail.com>
2026-09-07 01:11:29 +0000

    Refactor deck initialization and add Excalidraw diagram for project structure

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
+
+	cards := newDeck()
+
+	/*cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	/* ...
 	now we call this from the deck.go file instead.
 	for i, card := range cards {
 		fmt.Println(i, card)
@@ -14,6 +16,8 @@ func main() {
 	cards.print()
 }
 
+/*
 func newCard() string {
 	return "Five of Diamonds"
 }
+*/
